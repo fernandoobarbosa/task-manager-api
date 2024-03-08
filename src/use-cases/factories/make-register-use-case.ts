d185772d15a361d15927262c4878de015c26e5c2
Fernando Barbosa <fernandobarbosa1697@gmail.com>
2024-03-08 10:34:02 -0300

    change repository extension

--- src/use-cases/factories/make-register-use-case.ts
+++ src/use-cases/factories/make-register-use-case.ts
@@ -1,4 +1,4 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users.repository'
 import { RegisterUseCase } from '../register.use-case'
 
 export function makeRegisterUseCase() {
